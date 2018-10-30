bd3e63e522f551c7caf3bd373caece6c8b2adc77
joey banania <joeyb@magenic.com>
2018-10-31 05:39:22 +0800

    add order component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Order from './Components/Order/Order';
+
 class App extends Component {
   render() {
     return (
@@ -19,6 +21,8 @@ class App extends Component {
           >
             Learn React
           </a>
+
+          <Order/>
         </header>
       </div>
     );
---
+++ src/Components/Order/Order.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Order extends Component {
+    render () {
+        return (
+            <div>
+                <p>Place your Order here</p>
+            </div>
+        );
+    }
+}
+
+export default Order;
